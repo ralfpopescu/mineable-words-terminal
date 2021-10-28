fc93308b6300450c3873946bf54b56bb1a67a073
Ralf Popescu <ralf@landinglion.com>
2021-10-27 21:08:42 -0400

    web3

--- src/components/Terminal/Views/Links/index.tsx
+++ src/components/Terminal/Views/Links/index.tsx
@@ -8,6 +8,9 @@ flex-direction: column;
 
 export const Links = () => (
     <Column>
+       <a href="https://discord.com/invite/EVquaxg9WA">Discord</a>
        <a href="https://twitter.com/mineable_words">Twitter</a>
+       <a href="https://opensea.io/collection/mineablepunks">OpenSea</a>
+       <a href="https://etherscan.io/address/0x595a8974c1473717c4b5d456350cd594d9bda687">Contract</a>
     </Column>
 )
