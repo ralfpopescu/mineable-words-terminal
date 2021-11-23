fe7aa45567fc7b6e36a581edae8f93c0c23086c2
Ralf Popescu <ralf@landinglion.com>
2021-11-23 16:44:07 -0500

    typo

--- src/components/Terminal/Views/BountyHelp/index.tsx
+++ src/components/Terminal/Views/BountyHelp/index.tsx
@@ -1,26 +1,38 @@
-import { Line } from '../../../Line'
+import { Line } from "../../../Line";
 
-const Command = ({ children }: any) => <span style={{ color: 'yellow' }}>{children}</span>
+const Command = ({ children }: any) => <span style={{ color: "yellow" }}>{children}</span>;
 
 export const BountyHelp = () => (
-<div style={{ display: 'flex', flexDirection: 'column'}}>
+  <div style={{ display: "flex", flexDirection: "column" }}>
     <Command>bounties</Command>
     <span>Show all bounties currently offered.</span>
     <Line />
     <Command>bounty-offer [word] [offer]</Command>
-    <span>Offer a bounty in ETH for finding a word. Anyone who finds your word can claim the bounty, and you receive the mword.</span>
+    <span>
+      Offer a bounty in ETH for finding a word. Anyone who finds your word can claim the bounty, and
+      you receive the mword.
+    </span>
     <span> *** example: bounty-offer encyclopedia 1.5</span>
     <Line />
     <Command>bounty-claim [word]</Command>
-    <span>Claim a bounty with an mword that you own. Requires that you have minted the word and that a bounty exists for that word.</span>
+    <span>
+      Claim a bounty with an mword that you own. Requires that you have minted the word and that a
+      bounty exists for that word.
+    </span>
     <span>*** example: bounty-claim encyclopedia</span>
     <Line />
     <Command>bounty-remove-initiate [word]</Command>
-    <span>Initiate the transaction to get your funds returned from a bounty. After 100 blocks have passed, you may use command "bounty-remove-complete" to get your funds. This is to prevent the abuse where a bounty can be rescinded when a claim is observed.</span>
+    <span>
+      Initiate the transaction to get your funds returned from a bounty. After 100 blocks have
+      passed, you may use command "bounty-remove-complete" to get your funds. This is to prevent the
+      abuse where a bounty can be rescinded when a claim is observed.
+    </span>
     <span>*** example: bounty-remove-initate encyclopedia</span>
     <Line />
     <Command>bounty-remove-complete [word]</Command>
-    <span>Complete the return of your bounty funds after 100 blocks have passed since your initation.</span>
-    <span>*** example: bounty-remove-initate encyclopedia</span>
-</div>
-)
+    <span>
+      Complete the return of your bounty funds after 100 blocks have passed since your initation.
+    </span>
+    <span>*** example: bounty-remove-complete encyclopedia</span>
+  </div>
+);
